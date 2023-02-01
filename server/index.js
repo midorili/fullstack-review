c0605e3d0d652a83197256f15d8755cc9367ca3b
Midori <midorili998@gmail.com>
2023-02-01 14:50:21 -0800

    "Complete post to repos endpoint"

--- server/index.js
+++ server/index.js
@@ -1,5 +1,13 @@
 const express = require('express');
 let app = express();
+const github = require('../helpers/github.js')
+const save = require('../server/index.js')
+const bodyParser = require("body-parser")
+const Repo = require('../database/index.js')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(express.static(__dirname + '/../client/dist'));
 
 // TODO - your code here!
 // Set up static file service for files in the `client/dist` directory.
@@ -7,6 +15,13 @@ let app = express();
 // this server must serve those files when requested.
 
 app.post('/repos', function (req, res) {
+  // console.log('req.term', term)
+  console.log('ok this is req', req.body)
+  var term = req.body.term
+  // console.log('alright this is res', res)
+  github.getReposByUsername(term)
+  res.send('Completed adding to MongoDB')
+
   // TODO - your code here!
   // This route should take the github username provided
   // and get the repo information from the github API, then
@@ -14,13 +29,20 @@ app.post('/repos', function (req, res) {
 });
 
 app.get('/repos', function (req, res) {
+  var term = req.body.term
+  console.log('repo', Repo)
+  Repo.Repo.find({ owner: 'octocat' }).sort({ stargazers_count: 1 }).limit(25)
+    .then(data => {
+      console.log('data', data)
+    })
+  res.send('hi')
   // TODO - your code here!
   // This route should send back the top 25 repos
 });
 
 let port = 1128;
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`listening on port ${port}`);
 });
 
